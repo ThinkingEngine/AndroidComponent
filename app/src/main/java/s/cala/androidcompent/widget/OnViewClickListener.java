cf55c11cd1c3d0c7aadaf0b01e0d2a555710b391
cala <623912264@qq.com>
2019-01-08 22:26:00 +0800

    添加apptitlebar

---
+++ app/src/main/java/s/cala/androidcompent/widget/OnViewClickListener.java
@@ -0,0 +1,11 @@
+package s.cala.androidcompent.widget;
+
+/**
+ * package name:s.cala.androidcompent.widget
+ * create:cala
+ * date:2019/1/8
+ * commits:
+ */
+public interface OnViewClickListener {
+    void onClick();
+}
